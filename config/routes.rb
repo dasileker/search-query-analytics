df7a92bdfec67e251e815aa93a35fc516609c7ee
dasileker <dasileker@gmail.com>
2023-04-09 22:18:51 +0000

    add sidekiq to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  require 'sidekiq/web'
+  mount Sidekiq::Web => '/sidekiq'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
