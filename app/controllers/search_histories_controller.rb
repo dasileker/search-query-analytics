fda3dfea8c5dd1286cfd49600a157aa88d5e2020
dasileker <dasileker@gmail.com>
2023-04-12 15:32:56 +0000

    added the new data

--- app/controllers/search_histories_controller.rb
+++ app/controllers/search_histories_controller.rb
@@ -2,12 +2,11 @@ class SearchHistoriesController < ApplicationController
   before_action :authenticate_user!
   
   def index
-    @search_histories = SearchHistory.order(count: :desc, created_at: :desc).all
+    @search_histories = SearchHistory.order(count: :desc, created_at: :desc).limit(nil)
   end
   
   def trends
-    @top_searches = current_user.search_histories.order(count: :desc).all
-
+    @top_searches = current_user.search_histories.order(count: :desc).limit(nil)
     respond_to do |format|
       format.html
       format.json { render json: @top_searches }
@@ -16,7 +15,7 @@ class SearchHistoriesController < ApplicationController
 
 
   def analytics
-    @top_searches = current_user.search_histories.order(count: :desc, created_at: :desc).limit(40)
+    @top_searches = current_user.search_histories.order(count: :desc, created_at: :desc).limit(nil)
   end
 
 
--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -6,23 +6,21 @@ class SearchesController < ApplicationController
   end
 
   def create
-    @query = params[:query].strip.downcase
-    @articles = Article.where("lower(title) LIKE ?", "%#{@query}%")
+    @query = params[:query]
+    @articles = Article.where("title LIKE ?", "%#{@query}%")
 
-    if @query.present? && @query.include?(" ")
-      search_history = current_user.search_histories.find_or_initialize_by(query: @query)
+    query_segments = @query.split(/\s+/)
+    query_segments.each do |segment|
+    search_history = current_user.search_histories.find_or_initialize_by(query: @query)
       if search_history.new_record?
         search_history.count = 1
       else
         search_history.count += 1
       end
       search_history.save
-    end
-    
-    @last_search = current_user.search_histories.order(created_at: :desc).first
-    respond_to do |format|
-      format.turbo_stream
-      format.html { redirect_to root_path }
+      
+      flash.now[:success] = "Search added successfully!" if @query.present?
+      @last_search = current_user.search_histories.order(created_at: :desc).first
     end
   end
 
